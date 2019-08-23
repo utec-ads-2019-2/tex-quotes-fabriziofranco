784922eaee2be43fd45937d3af549ba64c7ae0fc
Fabrizio David Franco Amayo <39867790+fabriziofranco@users.noreply.github.com>
2019-08-23 11:19:17 -0500

    Update main.cpp
    Probado en uva accepted

--- main.cpp
+++ main.cpp
@@ -1,7 +1,24 @@
 #include <iostream>
-
+#include <string>
+#include <algorithm>
 using namespace std;
 
-int main(int argc, char *argv[]) {
+int main() {
+    string quote;
+    int contador=0;
+    while (getline(cin, quote)){
+        if(quote.empty())
+            break;
+        for(int i=0 ; i<quote.size();i++){
+            if(quote[i]=='\"'){
+                if(contador==0)
+                    quote.replace(quote.find('\"'),1, "``");
+                else if(contador==1)
+                    quote.replace(quote.find('\"'),1, "\'\'");
+                contador++;
+                if(contador==2)
+                    contador=0;
+        }}
+    cout<<quote<<endl;}
     return 0;
 }
